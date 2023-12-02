abe54dea5e0f2377e924c6308681e3cf4b14e09f
Emmanuel Corrales <hire_me@emcorrales.com>
2023-12-03 03:04:11 +0800

    Use a different approach to accomodate file size filtering.

--- hello.cpp
+++ hello.cpp
@@ -1,46 +1,77 @@
 #include <QApplication>
-#include <QFileDialog>
-#include <QPushButton>
-#include <QVBoxLayout>
+#include <QFileSystemModel>
+#include <QTreeView>
+#include <QDir>
 
-class FileOpenDialog : public QWidget{
-    
+class CustomFileSystemModel : public QFileSystemModel
+{
 public:
-
-    FileOpenDialog(QWidget *parent = nullptr) : QWidget(parent) {
-        setupUi();
+    CustomFileSystemModel(QObject* parent = nullptr)
+    : QFileSystemModel(parent)
+    {
     }
     
-    private slots:
-    void openFile() {
-        QString filePath = QFileDialog::getOpenFileName(this, tr("Open File"), QDir::homePath(),
-                                                        tr("Sequencher Project Files (*.spf);;Common Assembly Format (*.caf);;All Readable (*.spf *.caf);;Smaller than 500kb;;500kb or larger"));
-        
-        if (!filePath.isEmpty()) {
-            // Handle the selected file
-            qDebug("Selected File: %s", qPrintable(filePath));
+    QVariant data(const QModelIndex& index, int role) const override
+    {
+        if (role == Qt::SizeHintRole) {
+            // Filter files based on size.
+            qint64 fileSize = fileInfo(index).size();
+            if (fileSize <= maxSize) {
+                return QFileSystemModel::data(index, role);
+            } else {
+                return QVariant();
+            }
         }
+        
+        return QFileSystemModel::data(index, role);
+    }
+    
+    Qt::ItemFlags flags(const QModelIndex& index) const override
+    {
+        Qt::ItemFlags defaultFlags = QFileSystemModel::flags(index);
+        
+        // Disable specific items (e.g., files with size > maxSize)
+        qint64 fileSize = fileInfo(index).size();
+        if (fileSize > maxSize) {
+            return defaultFlags & ~Qt::ItemIsEnabled;
+        }
+        
+        return defaultFlags;
+    }
+    
+    void setMaxSize(qint64 size)
+    {
+        maxSize = size;
     }
     
 private:
-    void setupUi() {
-        QVBoxLayout *layout = new QVBoxLayout(this);
-        
-        QPushButton *openButton = new QPushButton(tr("Open File"), this);
-        connect(openButton, &QPushButton::clicked, this, &FileOpenDialog::openFile);
-        layout->addWidget(openButton);
-
-        setLayout(layout);
-        setWindowTitle(tr("File Open Dialog"));
-    }
+    qint64 maxSize = -1;
 };
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
     
-    FileOpenDialog window;
-    window.show();
+    // Create a custom file system model
+    CustomFileSystemModel model;
+    
+    // Set the home path as default model.
+    QString homePath = QDir::homePath(); // Change this to your desired directory
+    model.setRootPath(homePath);
+    
+    // Create a tree view and set the model
+    QTreeView treeView;
+    treeView.setModel(&model);
+    
+    // Set the root index of the tree view to the root path index
+    QModelIndex homeIndex = model.index(homePath);
+    treeView.setRootIndex(homeIndex);
+    
+    // Set a maximum file size for filtering (adjust as needed)
+    model.setMaxSize(50 * 1024); // Filter files greater than 1 MB
+    
+    // Show the tree view
+    treeView.show();
     
     return app.exec();
 }
