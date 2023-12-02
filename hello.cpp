4f7a2552ceb4d5db20ac6120c527ea251d02627c
Emmanuel Corrales <hire_me@emcorrales.com>
2023-12-03 04:33:24 +0800

    Add a combobox at the bottom.

--- hello.cpp
+++ hello.cpp
@@ -3,6 +3,7 @@
 #include <QTreeView>
 #include <QDir>
 #include <QVBoxLayout>
+#include <QComboBox>
 
 class CustomFileSystemModel : public QFileSystemModel
 {
@@ -54,6 +55,9 @@ class MyWidget : public QWidget
 public:
     MyWidget(QWidget *parent = nullptr) : QWidget(parent)
     {
+        // Create a layout
+        QVBoxLayout *layout = new QVBoxLayout(this);
+        
         // Create a custom file system model
         CustomFileSystemModel *model = new CustomFileSystemModel(this);
         
@@ -71,13 +75,26 @@ public:
         
         // Set a maximum file size for filtering (adjust as needed)
         model->setMaxSize(50 * 1024); // Filter files greater than 1 MB
-        
-        // Create a layout and set the tree view
-        QVBoxLayout *layout = new QVBoxLayout(this);
         layout->addWidget(treeView);
+        
+        QComboBox *comboBox = new QComboBox(this);
+        comboBox->addItem("Sequencher Projects");
+        comboBox->addItem("CAF Projects");
+        comboBox->addItem("All Readable");
+        comboBox->addItem("Smaller than 500kb");
+        comboBox->addItem("500kb or larger");
+        connect(comboBox, QOverload<int>::of(&QComboBox::currentIndexChanged), this, &MyWidget::onComboBoxIndexChanged);
+        layout->addWidget(comboBox);
         
         // Set the layout for the widget
         setLayout(layout);
+    }
+    
+    private slots:
+    void onComboBoxIndexChanged(int index) {
+        int filterIndex = index;
+        // Handle the selection change
+        qDebug("Selected Index: %d", index);
     }
 };
 
